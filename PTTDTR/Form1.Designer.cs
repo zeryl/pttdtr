c3c8be6c19964a9e80a79bbb089e2fd1fd6c4279
Matt Carlson (Zeryl) <lordsaryon@gmail.com>
2014-05-03 15:50:43 -0500

    First code commit

---
+++ PTTDTR/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PTTDTR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(85, 69);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Ports";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Toggle DTR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(365, 43);
+            this.label1.MinimumSize = new System.Drawing.Size(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Off";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 70);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Focus Me!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ptt_event_handler_down);
+            this.button3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ptt_event_handler_up);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 101);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "PTT via DTR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PTTDTR/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTTDTR
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = new SerialPort();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] theSerialPortNames = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (string port in theSerialPortNames)
+            {
+                this.listBox1.Items.Add(port);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string comport = this.listBox1.SelectedItem.ToString();
+
+            if (port.IsOpen)
+            {
+                port.Close();
+            }
+
+            port = new SerialPort(comport);
+            try
+            {
+                port.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open port " + comport + ".  Please make sure it is not in use!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port.DtrEnable)
+            {
+                port.DtrEnable = false;
+                this.label1.Text = "Off";
+            }
+            else
+            {
+                port.DtrEnable = true;
+                this.label1.Text = "On";
+            }
+        }
+
+        private void ptt_event_handler_down(Object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                port.DtrEnable = true;
+                this.label1.Text = "On";
+            }
+        }
+
+        private void ptt_event_handler_up(Object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                port.DtrEnable = false;
+                this.label1.Text = "Off";
+            }
+        }
+    }
+}
